cee93371bfd2adf131a03156f740c4e5a3de4f8e
jandutkiewicz0309 <jan.dutkiewicz@op.pl>
2023-06-04 16:47:21 +0200

    create structure and stories

---
+++ sdk/packages/modal/src/components/nightly-header/nightly-header.ts
@@ -0,0 +1,32 @@
+import { customElement, property } from 'lit/decorators.js'
+import { TailwindElement } from '../../shared/tailwind.element'
+import style from './nightly-header.css?inline'
+import { html } from 'lit'
+import Logo from '../../static/svg/Logo.svg'
+import Close from '../../static/svg/Close.svg'
+
+@customElement('nightly-header')
+export class NightlyHeader extends TailwindElement(style) {
+  @property()
+  onClose = () => console.log()
+
+  render() {
+    return html`
+      <div class="mainContainer-header">
+        <div class="logoContainer">
+          <img class="header-logo" src=${Logo} />
+          <button class="closeButton">
+            <img src=${Close} this.onClose />
+          </button>
+        </div>
+        <div class="animation-header"></div>
+      </div>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'nightly-header': NightlyHeader
+  }
+}
