0516c49d8d8dd02c4c10048c2be32ae89d92ab47
Adrian <adrianwojciak@interia.pl>
2023-06-18 17:58:55 +0200

    ws bundling

--- sdk/apps/sui/rollup.config.js
+++ sdk/apps/sui/rollup.config.js
@@ -25,7 +25,6 @@ export default [
       '@mysten/wallet-standard',
       '@noble/hashes',
       '@nightlylabs/nightly-connect-base',
-      'isomorphic-ws',
       'uuid',
       'eventemitter3'
     ]
