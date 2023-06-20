de69a007b2b79689e0c9808e799db75e28bbab72
jandutkiewicz0309 <jan.dutkiewicz@op.pl>
2023-06-20 11:32:47 +0200

    add prop and style element

--- sdk/packages/modal/src/components/nightly-connect-wallet/nightly-connect-wallet.stories.ts
+++ sdk/packages/modal/src/components/nightly-connect-wallet/nightly-connect-wallet.stories.ts
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from '@storybook/web-components'
 import { html } from 'lit'
 import './nightly-connect-wallet'
 import { NightlyConnectWallet } from './nightly-connect-wallet'
+import binance from '../../static/svg/BinanceIcon.svg'
 
 const meta = {
   title: 'nightly-connect-wallet',
@@ -14,6 +15,13 @@ const meta = {
       <nightly-connect-wallet
         coinName=${args.coinName}
         nameLink=${args.nameLink}
+        ?connecting=${args.connecting}
+        walletIcon=${args.walletIcon}
+        ?connected=${args.connected}
+        .onClick=${args.onClick}
+        .onClose=${args.onClose}
+        .fallback=${args.fallback}
+        .clickLink=${args.clickLink}
       ></nightly-connect-wallet>
     `
   }
@@ -22,10 +30,31 @@ const meta = {
 export default meta
 type Story = StoryObj<NightlyConnectWallet>
 
-export const Default: Story = {
-  name: 'Default',
+export const Connecting: Story = {
+  name: 'Connecting',
   args: {
     coinName: 'Binance Wallet',
-    nameLink: 'Binance Wallet website'
+    nameLink: 'Binance Wallet website',
+    connecting: true,
+    walletIcon: binance,
+    connected: false,
+    onClose: () => console.log('close'),
+    onClick: () => console.log('try again click'),
+    fallback: () => console.log('back to main page'),
+    clickLink: () => console.log('user click link')
+  }
+}
+export const ConnectingFailed: Story = {
+  name: 'ConnectingFailed',
+  args: {
+    coinName: 'Binance Wallet',
+    nameLink: 'Binance Wallet website',
+    connecting: false,
+    walletIcon: binance,
+    connected: false,
+    onClose: () => console.log('close'),
+    onClick: () => console.log('try again click'),
+    fallback: () => console.log('back to main page'),
+    clickLink: () => console.log('user click link')
   }
 }
--- sdk/packages/modal/src/components/nightly-connect-wallet/nightly-connect-wallet.ts
+++ sdk/packages/modal/src/components/nightly-connect-wallet/nightly-connect-wallet.ts
@@ -3,55 +3,83 @@ import { TailwindElement } from '../../shared/tailwind.element'
 import style from './nightly-connect-wallet.css?inline'
 import { html } from 'lit'
 import '../nightly-header/nightly-header'
-import binance from '../../static/svg/BinanceIcon.svg'
-
+import vector from '../../static/svg/backButton.svg'
 @customElement('nightly-connect-wallet')
 export class NightlyConnectWallet extends TailwindElement(style) {
   @property({ type: Boolean })
   connecting = false
 
+  @property({ type: Boolean })
+  connected = false
+
   @property({ type: String })
   nameLink = ''
 
   @property({ type: String })
+  walletIcon = ''
+
+  @property({ type: String })
   coinName = ''
 
+  @property()
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onClick = () => {}
+
+  @property()
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onClose = () => {}
+
+  @property()
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  fallback = () => {}
+
+  @property()
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  clickLink = () => {}
+
   render() {
-    return html`
-      <nightly-header></nightly-header>
-      <div class="mainContainer">
-        <div class="headerContainer">
-          <div class="buttonContainer">
-            <button></button>
-          </div>
-          <div class="textContainer">
-            <span>Connect wallet</span>
-          </div>
-        </div>
-        <div class="coinInfoContainer">
-          <img src=${binance} />
-          <span class="coinName">${this.coinName}</span>
-          <div class="custom-loader"></div>
+    return this.connected
+      ? ''
+      : html`
+          <div class="mainContainer">
+            <nightly-header .onClose=${this.onClose}></nightly-header>
+            <div class="wrapperConnectPage">
+              <div class="headerContainer">
+                <div class="buttonContainer">
+                  <button @click=${this.fallback}>
+                    <img class="vector" src=${vector} />
+                  </button>
+                </div>
+                <div class="textContainer">
+                  <span>Connect wallet</span>
+                </div>
+              </div>
+              <div class="coinInfoContainer">
+                <img src=${this.walletIcon} />
+                <span class="coinName">${this.coinName}</span>
+                ${this.connecting
+                  ? html` <div class="connectingContainer">
+                      <span>Connecting... </span>
+                      <div class="custom-loader"></div>
+                    </div>`
+                  : html` <span class="error">Connecting failed</span> `}
+              </div>
 
-          <span class="connectInfo">Connecting failed</span>
-        </div>
-        <div class="reConnectWrapper">
-          <p>
-            Connecting takes too long? Make sure ${this.nameLink} is installed on your device.
-            Otherwise, visit
-            <a class="link" href="${this.nameLink}" @click=${this._handleClick}>${this.nameLink}</a>
-            to download it.
-          </p>
-          <button>Try again</button>
-        </div>
-      </div>
-    `
+              <div class="reConnectWrapper">
+                <p>
+                  Connecting takes too long? Make sure ${this.nameLink} is installed on your device.
+                  Otherwise, visit
+                  <a class="link" href="${this.nameLink}" @click=${this.clickLink}
+                    >${this.nameLink}</a
+                  >
+                  to download it.
+                </p>
+                <button @click=${this.onClick}>Try again</button>
+              </div>
+            </div>
+          </div>
+        `
   }
-  _handleClick() {
-    console.log('Link clicked')
-  }
-
-  //   showAllWallets() { @click=${this.showAllWallets}   <img src=${vector} />}
 }
 
 declare global {
