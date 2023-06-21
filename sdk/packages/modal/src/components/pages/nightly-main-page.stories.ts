d2515aa5d1b14fea03a81200a5b4eed5f30ba195
jandutkiewicz0309 <jan.dutkiewicz@op.pl>
2023-06-21 18:22:42 +0200

    create main page and stories

---
+++ sdk/packages/modal/src/components/pages/nightly-main-page.stories.ts
@@ -0,0 +1,127 @@
+import { Meta, StoryObj } from '@storybook/web-components'
+import { html } from 'lit'
+import { NightlyMainPage } from './nightly-main-page'
+import './nightly-main-page'
+import Phantom from '../../static/svg/PhantomIcon.svg'
+import MetaMask from '../../static/svg/MetaMaskIcon.svg'
+import Coinbase from '../../static/svg/CoinbaseIcon.svg'
+import Glow from '../../static/svg/GlowIcon.svg'
+import ZenGO from '../../static/svg/ZenGOIcon.svg'
+import Trust from '../../static/svg/TrustIcon.svg'
+import Binance from '../../static/svg/BinanceIcon.svg'
+import Sollet from '../../static/svg/SolletIcon.svg'
+import NightlyIcon from '../../static/svg/NightlyIcon.svg'
+import ChainIcon from '../../static/svg/ChainIcon.svg'
+
+const meta = {
+  title: 'nightly-main-page',
+  parameters: {
+    layout: 'centered'
+  },
+
+  render: (args) => {
+    return html`
+      <nightly-main-page
+        .onClose=${args.onClose}
+        .selectorItems=${args.selectorItems}
+        .onWalletClick=${args.onWalletClick}
+        .chainIcon=${args.chainIcon}
+        .chainName=${args.chainName}
+        .sessionId=${args.sessionId}
+        .network=${args.network}
+        coinName=${args.coinName}
+        nameLink=${args.nameLink}
+        ?connecting=${args.connecting}
+        walletIcon=${args.walletIcon}
+        ?connected=${args.connected}
+        .tryAgainClick=${args.tryAgainClick}
+        .onClose=${args.onClose}
+        .fallback=${args.fallback}
+        link=${args.link}
+        ?openWalletConncet=${args.openWalletConncet}
+      ></nightly-main-page>
+    `
+  }
+} satisfies Meta<NightlyMainPage>
+
+export default meta
+
+interface WalletSelectorItem {
+  name: string
+  icon: string
+  status: string
+}
+
+interface NightlyModalArgs {
+  onClose: () => void
+  selectorItems: WalletSelectorItem[]
+  onWalletClick: (name: string) => void
+  chainIcon: string
+  chainName: string
+  sessionId: string
+  network: string
+  coinName: string
+  nameLink: string
+  connecting: boolean
+  walletIcon: string
+  connected: boolean
+  tryAgainClick: () => void
+  fallback: () => void
+  link: string
+  openWalletConncet: boolean
+}
+type Story = StoryObj<NightlyModalArgs>
+
+export const Default: Story = {
+  name: 'Default',
+  args: {
+    onClose: () => console.log('close'),
+    selectorItems: [
+      { name: 'Phantom', icon: Phantom, status: 'recent' },
+      { name: 'Nightly Wallet', icon: NightlyIcon, status: 'recent' },
+      { name: 'MetaMask', icon: MetaMask, status: '' },
+      { name: 'Glow', icon: Glow, status: '' },
+      { name: 'ZenGO', icon: ZenGO, status: 'detected' },
+      { name: 'Trust', icon: Trust, status: '' },
+      { name: 'Binance', icon: Binance, status: '' },
+      { name: 'Sollet', icon: Sollet, status: '' },
+      { name: 'Phantom', icon: Phantom, status: '' },
+      { name: 'MetaMask', icon: MetaMask, status: 'recent' },
+      { name: 'Coinbase', icon: Coinbase, status: '' },
+      { name: 'ZenGO', icon: ZenGO, status: '' },
+      { name: 'Trust', icon: Trust, status: 'detected' },
+      { name: 'Binance', icon: Binance, status: '' },
+      { name: 'Phantom', icon: Phantom, status: 'recent' },
+      { name: 'Nightly Wallet', icon: NightlyIcon, status: 'recent' },
+      { name: 'MetaMask', icon: MetaMask, status: '' },
+      { name: 'Glow', icon: Glow, status: '' },
+      { name: 'ZenGO', icon: ZenGO, status: 'detected' },
+      { name: 'Trust', icon: Trust, status: '' },
+      { name: 'Binance', icon: Binance, status: '' },
+      { name: 'Sollet', icon: Sollet, status: '' },
+      { name: 'Phantom', icon: Phantom, status: '' },
+      { name: 'MetaMask', icon: MetaMask, status: 'recent' },
+      { name: 'Coinbase', icon: Coinbase, status: '' },
+      { name: 'ZenGO', icon: ZenGO, status: '' },
+      { name: 'Trust', icon: Trust, status: 'detected' },
+      { name: 'Binance', icon: Binance, status: '' }
+    ],
+    onWalletClick: (name: string) => {
+      console.log('Item clicked:', name)
+    },
+    chainIcon: ChainIcon,
+    chainName: 'Solana',
+    sessionId:
+      'fsdhfdzfsdhgfzghggdfhbgchgbdfnvfbxhncvfjhzxdhgbhghfgfvzhfgjhgszdhgzxdfhgfzxdjfuhdfhgd',
+    network: 'SOLANA',
+    coinName: 'Binance Wallet',
+    nameLink: 'Binance Wallet website',
+    connecting: true,
+    walletIcon: Binance,
+    connected: false,
+    tryAgainClick: () => console.log('try again click'),
+    fallback: () => console.log('back to main page'),
+    link: `https://www.binance.com/en`,
+    openWalletConncet: true
+  }
+}
---
+++ sdk/packages/modal/src/components/pages/nightly-main-page.ts
@@ -0,0 +1,162 @@
+import { html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import { TailwindElement } from '../../shared/tailwind.element'
+import { Breakpoint, getBreakpointFromWidthInMainPage } from '../../utils/utils'
+import '../nightly-modal/nightly-modal'
+import style from './nightly-main-page.css?inline'
+import '../nightly-connect-wallet/nightly-connect-wallet'
+import '../nightly-wallet-selector-page/nightly-wallet-selector-small-page/nightly-wallet-selector-small-page'
+
+@customElement('nightly-main-page')
+export class NightlyMainPage extends TailwindElement(style) {
+  @property()
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onClose = () => {}
+
+  @property({ type: Array })
+  selectorItems = []
+
+  @property({ type: Boolean })
+  openWalletConncet = false
+
+  @property({ type: Function })
+  onWalletClick(name: string): void {
+    console.log('Item clicked:', name)
+  }
+  @property({ type: String })
+  chainIcon = ''
+
+  @property({ type: String })
+  chainName = ''
+
+  @property({ type: String })
+  sessionId = ''
+
+  @property({ type: String })
+  network = ''
+
+  @property({ type: String })
+  copyMessage = 'Copy'
+
+  @property({ type: Boolean })
+  connecting = false
+
+  @property({ type: Boolean })
+  connected = false
+
+  @property({ type: String })
+  nameLink = ''
+
+  @property({ type: String })
+  link = ''
+
+  @property({ type: String })
+  walletIcon = ''
+
+  @property({ type: String })
+  coinName = ''
+
+  @property()
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  fallback = () => {}
+
+  @property()
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  tryAgainClick = () => {}
+
+  timeoutRef: number | undefined = undefined
+
+  breakpoint: Breakpoint
+
+  constructor() {
+    super()
+    this.onWalletClick = this.onWalletClick.bind(this)
+    this.openConnectWallet = this.openConnectWallet.bind(this)
+    this.breakpoint = 'sm'
+    this.updateBreakpoint()
+    this.resizeListener()
+  }
+
+  updateBreakpoint() {
+    const screenWidth = window.innerWidth
+    this.breakpoint = getBreakpointFromWidthInMainPage(screenWidth)
+  }
+
+  resizeListener() {
+    window.addEventListener('resize', () => {
+      this.updateBreakpoint()
+      this.requestUpdate()
+    })
+  }
+
+  render() {
+    if (this.openWalletConncet) {
+      return this.renderConnectWallet()
+    }
+    if (this.breakpoint === 'xs') {
+      return html`
+        <nightly-wallet-selector-small-page
+          .breakpoint=${this.breakpoint}
+          .hasUpdated=${this.hasUpdated}
+          .isUpdatePending=${this.isUpdatePending}
+          .network=${this.network}
+          .onWalletClick=${this.openConnectWallet}
+          .onClose=${this.onClose}
+          .selectorItems=${this.selectorItems}
+          .sessionId=${this.sessionId}
+        ></nightly-wallet-selector-small-page>
+      `
+    } else {
+      return html`
+        <div>
+          <nightly-modal
+            .chainIcon=${this.chainIcon}
+            .chainName=${this.chainName}
+            .copyMessage=${this.copyMessage}
+            .hasUpdated=${this.hasUpdated}
+            .isUpdatePending=${this.isUpdatePending}
+            .network=${this.network}
+            .onClose=${this.onClose}
+            .onWalletClick=${this.openConnectWallet}
+            .selectorItems=${this.selectorItems}
+            .sessionId=${this.sessionId}
+          ></nightly-modal>
+        </div>
+      `
+    }
+  }
+
+  openConnectWallet() {
+    this.openWalletConncet = true
+    this.requestUpdate()
+  }
+
+  backToPage = () => {
+    this.openWalletConncet = false
+    this.requestUpdate()
+  }
+
+  renderConnectWallet() {
+    return html`
+      <div class="">
+        <nightly-connect-wallet
+          .breakpoint=${this.breakpoint}
+          .coinName=${this.coinName}
+          .connecting=${this.connecting}
+          .onClose=${this.onClose}
+          .tryAgainClick=${this.tryAgainClick}
+          .fallback=${this.backToPage}
+          .link=${this.link}
+          .nameLink=${this.nameLink}
+          .walletIcon=${this.walletIcon}
+        ></nightly-connect-wallet>
+      </div>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'nightly-main-page': NightlyMainPage
+  }
+}
