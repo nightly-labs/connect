48fff4c5fa0d3eeb3f046d33e05d07de70bf65b9
NB😈 <norbertbodziony@gmail.com>
2023-06-24 21:22:43 +0200

    fix cors

--- server/src/utils.rs
+++ server/src/utils.rs
@@ -1,7 +1,25 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use hyper::{header, Method};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn get_timestamp_in_milliseconds() -> u64 {
     let now = SystemTime::now();
     let since_the_epoch = now.duration_since(UNIX_EPOCH).expect("Time went backwards");
     since_the_epoch.as_millis() as u64
+}
+pub fn get_cors() -> CorsLayer {
+    CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
+        .allow_origin(Any)
+        .allow_headers(vec![
+            header::AUTHORIZATION,
+            header::ACCEPT,
+            header::CONTENT_TYPE,
+        ])
+        .max_age(Duration::from_secs(86400))
+        .vary(vec![
+            header::ORIGIN,
+            header::ACCESS_CONTROL_REQUEST_METHOD,
+            header::ACCESS_CONTROL_REQUEST_HEADERS,
+        ])
 }
