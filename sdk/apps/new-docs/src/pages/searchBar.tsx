37e90c5bf9dabe1ec077cca739e94c5b35dadf54
belvitadream <ganna.syrotenko@gmail.com>
2023-06-30 08:58:17 +0200

    init

---
+++ sdk/apps/new-docs/src/pages/searchBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocSearch } from '@docsearch/react'
+import '@docsearch/css'
+
+function App() {
+  return (
+    <DocSearch
+      appId="R2IYF7ETH7"
+      indexName="599cec31baffa4868cae4e79f180729b"
+      apiKey="docsearch" // connect
+    />
+  )
+}
+
+export default App
