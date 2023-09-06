4b04210baeee6609b6b445d5f8dc308315e28ae1
Adrian <adrianwojciak@interia.pl>
2023-09-06 21:03:19 +0200

    override styles, version 1

--- sdk/packages/modal/src/shared/tailwind.element.ts
+++ sdk/packages/modal/src/shared/tailwind.element.ts
@@ -2,4 +2,14 @@ import { unsafeCSS } from 'lit'
 
 import style from './tailwind.global.css'
 
-export const tailwindElement = (customStyle: string) => [unsafeCSS(style), unsafeCSS(customStyle)]
+let _stylesOverride = ''
+
+export const setStylesOverride = (override: string) => {
+  _stylesOverride = override
+}
+
+export const tailwindElement = (customStyle: string) => [
+  unsafeCSS(style),
+  unsafeCSS(customStyle),
+  unsafeCSS(_stylesOverride)
+]
