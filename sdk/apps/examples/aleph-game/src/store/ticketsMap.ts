b84e823b6cd125d191af9d6fd42d494133b5c93a
Pawel-Szydlo <pawel.szydlo.99@gmail.com>
2023-09-21 07:39:59 +0200

    change path task

--- sdk/apps/examples/aleph-game/src/store/ticketsMap.ts
+++ sdk/apps/examples/aleph-game/src/store/ticketsMap.ts
@@ -1,14 +1,14 @@
 export const TICKETS_MAP = {
   // 9 random tickets to index
-  uh3Rccil: 1,
-  a8UISBYU: 2,
-  NFKZcvMB: 3,
-  '5ryVaSo0': 4,
-  z8aiJmga: 5,
-  '8wGsgPHa': 6,
-  rK0IQ0LJ: 7,
-  H6kwvuqg: 8,
-  eibElg0N: 9
+  Oq6PjIHod9: 1,
+  '3l9VgG9uhu': 2,
+  Wa8tOKb9FL: 3,
+  PU6vP65fu2: 4,
+  abvYiMHOSa: 5,
+  FKEzmQy0Pk: 6,
+  UACtREt7st: 7,
+  Kox6D1FAj1: 8,
+  a8vSAdyEmE: 9
 }
 export type TicketId = keyof typeof TICKETS_MAP
 export type TicketValue = typeof TICKETS_MAP[TicketId]
@@ -16,12 +16,12 @@ export type TicketsMapType = {
   TicketId?: number
 }
 
-// https://aleph.game.nightly.app/ticket/uh3Rccil
-// https://aleph.game.nightly.app/ticket/a8UISBYU
-// https://aleph.game.nightly.app/ticket/NFKZcvMB
-// https://aleph.game.nightly.app/ticket/5ryVaSo0
-// https://aleph.game.nightly.app/ticket/z8aiJmga
-// https://aleph.game.nightly.app/ticket/8wGsgPHa
-// https://aleph.game.nightly.app/ticket/rK0IQ0LJ
-// https://aleph.game.nightly.app/ticket/H6kwvuqg
-// https://aleph.game.nightly.app/ticket/eibElg0N
+// http://near.game.nightly.app/ticket/Oq6PjIHod9
+// http://near.game.nightly.app/ticket/3l9VgG9uhu
+// http://near.game.nightly.app/ticket/Wa8tOKb9FL
+// http://near.game.nightly.app/ticket/PU6vP65fu2
+// http://near.game.nightly.app/ticket/abvYiMHOSa
+// http://near.game.nightly.app/ticket/FKEzmQy0Pk
+// http://near.game.nightly.app/ticket/UACtREt7st
+// http://near.game.nightly.app/ticket/Kox6D1FAj1
+// http://near.game.nightly.app/ticket/a8vSAdyEmE
