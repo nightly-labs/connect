ac939680e306398548d0356cbfa298688c86528f
NB😈 <norbertbodziony@gmail.com>
2023-10-12 07:05:41 +0200

    add new wallets

--- server/src/wallets.rs
+++ server/src/wallets.rs
@@ -130,5 +130,109 @@ pub static WALLETS_METADATA: Lazy<Vec<WalletMetadata>> = Lazy::new(|| {
             version: Version("0.1.0".to_string()),
             wallet_type: WalletType::extension,
         },
+         // Subwallet
+         WalletMetadata {
+            slug: "subwallet-js".to_string(),
+            name: "subwallet-js".to_string(),
+            description: "Subwallet".to_string(),
+            homepage: "https://www.subwallet.app/".to_string(),
+            app: HashMap::from([
+                (
+                    Platform::chrome,
+                    "https://chrome.google.com/webstore/detail/subwallet-polkadot-wallet/onhogfjeacnfoofkfgppdlbmlmnplgbn".to_string(),
+                ),
+                (
+                    Platform::edge,
+                    "https://chrome.google.com/webstore/detail/subwallet-polkadot-wallet/onhogfjeacnfoofkfgppdlbmlmnplgbn".to_string(),
+                ),
+                (
+                    Platform::browser,
+                    "https://chrome.google.com/webstore/detail/subwallet-polkadot-wallet/onhogfjeacnfoofkfgppdlbmlmnplgbn".to_string(),
+                ),
+                (
+                    Platform::brave,
+                    "https://chrome.google.com/webstore/detail/subwallet-polkadot-wallet/onhogfjeacnfoofkfgppdlbmlmnplgbn".to_string(),
+                ),
+                (
+                    Platform::opera,
+                    "https://chrome.google.com/webstore/detail/subwallet-polkadot-wallet/onhogfjeacnfoofkfgppdlbmlmnplgbn".to_string(),
+                ),
+                (
+                    Platform::opera,
+                    "https://chrome.google.com/webstore/detail/subwallet-polkadot-wallet/onhogfjeacnfoofkfgppdlbmlmnplgbn".to_string(),
+                ),
+                (
+                    Platform::firefox,
+                    "https://addons.mozilla.org/en-US/firefox/addon/subwallet/".to_string(),
+                ),
+            ]),
+            chains: vec![Network::new("polkadot")],
+            desktop: None,
+            mobile: None,
+            image: Images {
+                default: format!("https://registry.nightly.app/wallets/subwallet-js/default.png"),
+                sm: format!("https://registry.nightly.app/wallets/subwallet-js/default.png"),
+                md: format!("https://registry.nightly.app/wallets/subwallet-js/default.png"),
+                lg: format!("https://registry.nightly.app/wallets/subwallet-js/default.png"),
+            },
+            inject_path: HashMap::from([
+                (Network::new("polkadot"),"window.injectedWeb3.subwallet-js".to_string()),
+            ]),
+            last_updated_timestamp: 1696942859,
+            version: Version("0.1.0".to_string()),
+            wallet_type: WalletType::extension,
+        },
+          // Talisman
+          WalletMetadata {
+            slug: "talisman".to_string(),
+            name: "talisman".to_string(),
+            description: "Talisman".to_string(),
+            homepage: "https://www.talisman.xyz/".to_string(),
+            app: HashMap::from([
+                (
+                    Platform::chrome,
+                    "https://chrome.google.com/webstore/detail/talisman-polkadot-and-eth/fijngjgcjhjmmpcmkeiomlglpeiijkld".to_string(),
+                ),
+                (
+                    Platform::edge,
+                    "https://chrome.google.com/webstore/detail/talisman-polkadot-and-eth/fijngjgcjhjmmpcmkeiomlglpeiijkld".to_string(),
+                ),
+                (
+                    Platform::browser,
+                    "https://chrome.google.com/webstore/detail/talisman-polkadot-and-eth/fijngjgcjhjmmpcmkeiomlglpeiijkld".to_string(),
+                ),
+                (
+                    Platform::brave,
+                    "https://chrome.google.com/webstore/detail/talisman-polkadot-and-eth/fijngjgcjhjmmpcmkeiomlglpeiijkld".to_string(),
+                ),
+                (
+                    Platform::opera,
+                    "https://chrome.google.com/webstore/detail/talisman-polkadot-and-eth/fijngjgcjhjmmpcmkeiomlglpeiijkld".to_string(),
+                ),
+                (
+                    Platform::opera,
+                    "https://chrome.google.com/webstore/detail/talisman-polkadot-and-eth/fijngjgcjhjmmpcmkeiomlglpeiijkld".to_string(),
+                ),
+                (
+                    Platform::firefox,
+                    "https://addons.mozilla.org/en-US/firefox/addon/talisman-wallet-extension".to_string(),
+                ),
+            ]),
+            chains: vec![Network::new("polkadot")],
+            desktop: None,
+            mobile: None,
+            image: Images {
+                default: format!("https://registry.nightly.app/wallets/talisman/default.png"),
+                sm: format!("https://registry.nightly.app/wallets/talisman/default.png"),
+                md: format!("https://registry.nightly.app/wallets/talisman/default.png"),
+                lg: format!("https://registry.nightly.app/wallets/talisman/default.png"),
+            },
+            inject_path: HashMap::from([
+                (Network::new("polkadot"),"window.injectedWeb3.talisman".to_string()),
+            ]),
+            last_updated_timestamp: 1696942859,
+            version: Version("0.1.0".to_string()),
+            wallet_type: WalletType::extension,
+        },
     ];
 });
