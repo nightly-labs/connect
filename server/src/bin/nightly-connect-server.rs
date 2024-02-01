e95445d63d578011cb211a49420ca709c40ad689
“Giems” <hubert.wabia@gmail.com>
2024-02-01 14:13:52 +0100

    bump axum

--- server/src/bin/nightly-connect-server.rs
+++ server/src/bin/nightly-connect-server.rs
@@ -1,6 +1,5 @@
 use server::router::get_router;
 use std::net::SocketAddr;
-use std::str::FromStr;
 use std::sync::mpsc::channel;
 
 #[tokio::main]
@@ -8,14 +7,20 @@ async fn main() {
     dotenvy::dotenv().expect(".env file not found");
 
     let router = get_router().await;
-    let socket = SocketAddr::from_str("127.0.0.1:6969").unwrap();
+    let listener = tokio::net::TcpListener::bind(&"127.0.0.1:6969")
+        .await
+        .expect("Failed to bind socket");
 
-    let server = axum::Server::bind(&socket).serve(
+    let server = axum::serve(
+        listener,
         router
             .clone()
             .into_make_service_with_connect_info::<SocketAddr>(),
     );
-    tokio::spawn(server);
+
+    tokio::spawn(async move {
+        server.await.unwrap();
+    });
 
     let (tx, rx) = channel();
     ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
