0d515bb6a62c8fd0e9cc4eb72f60f7ebb0a0673b
Kamil Czacharowski <72619640+MekLiii@users.noreply.github.com>
2024-02-13 22:13:29 +0100

    Removed unused code

--- sdk/packages/modal/src/components/nightly-footer/nightly-footer.ts
+++ sdk/packages/modal/src/components/nightly-footer/nightly-footer.ts
@@ -1,34 +1,23 @@
-import { customElement, property } from 'lit/decorators.js'
+import { customElement } from 'lit/decorators.js'
 import { tailwindElement } from '../../shared/tailwind.element'
 import style from './nightly-footer.css'
 import { LitElement, html } from 'lit'
 
 @customElement('nightly-footer')
 export class NightlyFooter extends LitElement {
-  @property()
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  onClose = () => {}
-
   static styles = tailwindElement(style)
-  
 
   render() {
     return html`
       <div class="nc_footerWrapper">
         <div class="nc_footerLogo"></div>
         <div class="nc_footerContent">
-          <p 
-            class="nc_footerText"
-          >
+          <p class="nc_footerText">
             By connecting, you agree to Common's
 
-            <a href="" target="_blank" class="nc_footerLink"
-              >Terms of Service</a
-            >
+            <a href="" target="_blank" class="nc_footerLink">Terms of Service</a>
             and to its
-            <a href="" target="_blank" class="nc_footerLink"
-              >Privacy Policy</a
-            >
+            <a href="" target="_blank" class="nc_footerLink">Privacy Policy</a>
           </p>
         </div>
       </div>
