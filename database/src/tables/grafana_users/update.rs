381863174e6993a4f7048fc5b280c27719291cac
“Giems” <hubert.wabia@gmail.com>
2024-02-27 14:09:01 +0100

    fix test

--- database/src/tables/grafana_users/update.rs
+++ database/src/tables/grafana_users/update.rs
@@ -54,9 +54,11 @@ impl Db {
 
 #[cfg(test)]
 mod tests {
-    use crate::tables::sessions::table_struct::DbNcSession;
+    use crate::{
+        structs::privelage_level::PrivilegeLevel,
+        tables::{grafana_users::table_struct::GrafanaUser, utils::to_microsecond_precision},
+    };
     use sqlx::types::chrono::Utc;
-    use std::time::Duration;
 
     #[tokio::test]
     async fn test_create_user() {
@@ -71,70 +73,19 @@ mod tests {
             .await
             .unwrap();
 
-        let now = Utc::now();
-        let start_of_first_period = now - Duration::from_secs(60 * 60 * 24 * 60); // Start of first period, 60 days ago
-        let start_of_second_period = now - Duration::from_secs(60 * 60 * 24 * 30); // Start of second period, 30 days ago
-        let num_sessions_first_period: u64 = 40;
-        let num_sessions_second_period: u64 = 28;
+        let user = GrafanaUser {
+            name: "test_user".to_string(),
+            team_id: team_id.to_string(),
+            team_admin: false,
+            email: "test_user_email".to_string(),
+            password_hash: "test_password_hash".to_string(),
+            privilege_level: PrivilegeLevel::Read,
+            creation_timestamp: to_microsecond_precision(&Utc::now()),
+        };
 
-        // Generate and save sessions for the first period
-        for i in 0..num_sessions_first_period {
-            let session_start = start_of_first_period
-                + Duration::from_secs(i * 86400 / num_sessions_first_period as u64);
-            let session_end = session_start + Duration::from_secs(60 * 30); // Duration of 30 minutes for each session
+        db.add_new_user(&user).await.unwrap();
 
-            let session = DbNcSession {
-                session_id: format!("session_{}_{}", app_id, i),
-                app_id: app_id.clone(),
-                app_metadata: "test_metadata".to_string(),
-                app_ip_address: "127.0.0.1".to_string(),
-                persistent: false,
-                network: "test_network".to_string(),
-                client: None,
-                session_open_timestamp: session_start,
-                session_close_timestamp: Some(session_end),
-            };
-
-            db.save_new_session(&session).await.unwrap();
-            db.close_session(&session.session_id, session_end)
-                .await
-                .unwrap();
-        }
-
-        // Generate and save sessions for the second period
-        for i in 0..num_sessions_second_period {
-            let session_start = start_of_second_period
-                + Duration::from_secs(i * 86400 / num_sessions_second_period as u64);
-            let session_end = session_start + Duration::from_secs(60 * 30); // Duration of 30 minutes for each session
-
-            let session = DbNcSession {
-                session_id: format!("session_{}_{}_2nd", app_id, i), // Ensure unique session IDs for the second period
-                app_id: app_id.clone(),
-                app_metadata: "test_metadata".to_string(),
-                app_ip_address: "127.0.0.1".to_string(),
-                persistent: false,
-                network: "test_network".to_string(),
-                client: None,
-                session_open_timestamp: session_start,
-                session_close_timestamp: Some(session_end),
-            };
-
-            db.save_new_session(&session).await.unwrap();
-            db.close_session(&session.session_id, session_end)
-                .await
-                .unwrap();
-        }
-
-        // Manually refresh the continuous aggregates
-        db.refresh_continuous_aggregates(vec![
-            "sessions_stats_per_app_daily".to_string(),
-            "sessions_stats_per_app_monthly".to_string(),
-        ])
-        .await
-        .unwrap();
-
-        let stats = db.get_monthly_sessions_stats(&app_id).await.unwrap();
-
-        assert_eq!(stats.len(), 2);
+        let user_result = db.get_user_by_user_name(&user.name).await.unwrap();
+        assert_eq!(user_result, user);
     }
 }
