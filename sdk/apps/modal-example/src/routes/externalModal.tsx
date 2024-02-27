44295fbef201cf2f83993edf1f68d5d411a60392
LukassF <szkulkerolf@gmail.com>
2024-02-27 12:43:11 +0100

    fix: update wallets list on disconnect

--- sdk/apps/modal-example/src/routes/externalModal.tsx
+++ sdk/apps/modal-example/src/routes/externalModal.tsx
@@ -48,6 +48,7 @@ export default function SolanaExternalModal() {
     })
 
     adapter.on('disconnect', () => {
+      modal.walletsList = adapter.walletsList
       setPublicKey(undefined)
     })
 
