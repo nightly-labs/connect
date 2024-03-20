a5a3e406ef901ca51fbba28d60a5e32241d4c8dc
“Giems” <hubert.wabia@gmail.com>
2024-03-20 09:17:41 +0100

    replace todo's in event handler with new event processors

--- server/src/http/cloud/events/events_handler.rs
+++ server/src/http/cloud/events/events_handler.rs
@@ -3,6 +3,11 @@ use super::{
     processors::{
         process_event_app_connect::process_event_app_connect,
         process_event_app_disconnect::process_event_app_disconnect,
+        process_event_client_connect_init::process_event_client_connect_init,
+        process_event_client_connect_resolve::process_event_client_connect_resolve,
+        process_event_client_disconnect::process_event_client_disconnect,
+        process_event_new_request::process_event_new_request,
+        process_event_request_resolved::process_event_request_resolved,
     },
 };
 use crate::{
@@ -34,10 +39,28 @@ pub async fn process_event(
         EventData::AppDisconnect(event) => {
             process_event_app_disconnect(event, &event_payload.app_id, ip, db_connection).await;
         }
-        EventData::ClientConnectInit(event) => todo!(),
-        EventData::ClientConnectResolve(event) => todo!(),
-        EventData::ClientDisconnect(event) => todo!(),
-        EventData::NewRequest(event) => todo!(),
-        EventData::RequestResolved(event) => todo!(),
+        EventData::ClientConnectInit(event) => {
+            process_event_client_connect_init(
+                event,
+                &event_payload.app_id,
+                ip,
+                db_connection,
+                geo_loc_requester,
+            )
+            .await;
+        }
+        EventData::ClientConnectResolve(event) => {
+            process_event_client_connect_resolve(event, &event_payload.app_id, ip, db_connection)
+                .await;
+        }
+        EventData::ClientDisconnect(event) => {
+            process_event_client_disconnect(event, &event_payload.app_id, ip, db_connection).await;
+        }
+        EventData::NewRequest(event) => {
+            process_event_new_request(event, &event_payload.app_id, ip, db_connection).await;
+        }
+        EventData::RequestResolved(event) => {
+            process_event_request_resolved(event, &event_payload.app_id, ip, db_connection).await;
+        }
     }
 }
