9a9f142909cd675de5012587a4340861557b38fc
NB😈 <norbertbodziony@gmail.com>
2024-03-20 13:39:58 +0100

    add vara support

--- sdk/packages/selector-polkadot/src/utils.ts
+++ sdk/packages/selector-polkadot/src/utils.ts
@@ -1,16 +1,21 @@
 import { NetworkData } from '@nightlylabs/wallet-selector-base'
-export type SupportedNetworks = 'Polkadot' | 'AlephZero'
+export type SupportedNetworks = 'Polkadot' | 'AlephZero' | 'Vara'
 export const networkToData = (network: SupportedNetworks): NetworkData => {
   switch (network) {
     case 'Polkadot':
       return {
         name: network,
-        icon: 'https://registry.nightly.app/networks/polkadot.png' // TODO add polka icon
+        icon: 'https://registry.nightly.app/networks/polkadot.png'
       }
     case 'AlephZero':
       return {
         name: network,
-        icon: 'https://registry.nightly.app/networks/alephzero.png' // TODO add polka icon
+        icon: 'https://registry.nightly.app/networks/alephzero.png'
+      }
+    case 'Vara':
+      return {
+        name: network,
+        icon: 'https://registry.nightly.app/networks/vara.png'
       }
   }
 }
