c39f2cf50dda81b14e2688d51e95e3cd391dde7d
“Giems” <hubert.wabia@gmail.com>
2024-03-21 14:12:03 +0100

    bump

--- server/src/http/cloud/events/processors/process_event_change_network.rs
+++ server/src/http/cloud/events/processors/process_event_change_network.rs
@@ -9,53 +9,29 @@ pub async fn process_event_change_network(
     db: &Arc<Db>,
 ) {
     // Establish a new transaction
-    match db.connection_pool.begin().await {
-        Ok(mut tx) => {
-            // Create a new event index in the database
-            match db
-                .create_new_event_entry(&mut tx, &app_id, &EventType::ChangeNetwork)
-                .await
-            {
-                Ok(event_id) => {
-                    // Now create a new event change network corresponding to the event
-                    match db
-                        .create_new_event_change_network(
-                            &mut tx,
-                            event_id,
-                            &event.session_id,
-                            &event.request_id,
-                            &event.old_network,
-                        )
-                        .await
-                    {
-                        Ok(_) => {
-                            // Commit the transaction
-                            if let Err(err) = tx.commit().await {
-                                error!(
-                                    "Failed to commit transaction for new change network event, app_id: [{}], event: [{:?}], err: [{}]",
-                                    app_id, event, err
-                                );
-                            }
+    let mut tx = match db.connection_pool.begin().await {
+        Ok(tx) => tx,
+        Err(err) => {
+            error!(
+                "Failed to create new transaction to save change network event, app_id: [{}], event: [{:?}], err: [{}]",
+                app_id, event, err
+            );
+            return;
+        }
+    };
 
-                            return;
-                        }
-                        Err(err) => {
-                            error!(
-                                "Failed to create new change network event, app_id: [{}], event: [{:?}], err: [{}]",
-                                app_id, event, err
-                            );
-                        }
-                    }
-                }
-                Err(err) => {
-                    error!(
-                        "Failed to create new event index, app_id: [{}], event: [{:?}], err: [{}]",
-                        app_id, event, err
-                    );
-                }
-            }
+    // Create a new event index
+    let event_id = match db
+        .create_new_event_entry(&mut tx, &app_id, &EventType::ChangeNetwork)
+        .await
+    {
+        Ok(event_id) => event_id,
+        Err(err) => {
+            error!(
+                "Failed to create new event index, app_id: [{}], event: [{:?}], err: [{}]",
+                app_id, event, err
+            );
 
-            // If we have not returned yet, then we have failed to save the event
             // Rollback the transaction
             if let Err(err) = tx.rollback().await {
                 error!(
@@ -63,12 +39,44 @@ pub async fn process_event_change_network(
                     app_id, event, err
                 );
             }
+
+            return;
+        }
+    };
+
+    // Now create a new event change network corresponding to the event
+    match db
+        .create_new_event_change_network(
+            &mut tx,
+            event_id,
+            &event.session_id,
+            &event.request_id,
+            &event.old_network,
+        )
+        .await
+    {
+        Ok(_) => {
+            // Commit the transaction
+            if let Err(err) = tx.commit().await {
+                error!(
+                    "Failed to commit transaction for new change network event, app_id: [{}], event: [{:?}], err: [{}]",
+                    app_id, event, err
+                );
+            }
         }
         Err(err) => {
             error!(
-                "Failed to create new transaction to save change network event, app_id: [{}], event: [{:?}], err: [{}]",
+                "Failed to create new change network event, app_id: [{}], event: [{:?}], err: [{}]",
                 app_id, event, err
             );
+
+            // Rollback the transaction
+            if let Err(err) = tx.rollback().await {
+                error!(
+                    "Failed to rollback transaction for new change network event, app_id: [{}], event: [{:?}], err: [{}]",
+                    app_id, event, err
+                );
+            }
         }
     }
 }
--- server/src/http/cloud/events/processors/process_event_change_network_resolve.rs
+++ server/src/http/cloud/events/processors/process_event_change_network_resolve.rs
@@ -9,50 +9,49 @@ pub async fn process_event_change_network_resolve(
     db: &Arc<Db>,
 ) {
     // Establish a new transaction
-    match db.connection_pool.begin().await {
-        Ok(mut tx) => {
-            // Update the change network event in the database
-            match db
-                .update_event_change_network(
-                    &mut tx,
-                    &event.request_id,
-                    RequestStatus::from(&event.failure_reason),
-                    &event.new_network,
-                )
-                .await
-            {
-                Ok(_) => {
-                    // Commit the transaction
-                    if let Err(err) = tx.commit().await {
-                        error!(
-                                "Failed to commit transaction for update change network event, app_id: [{}], event: [{:?}], err: [{}]",
-                                app_id, event, err
-                            );
-                    }
-                }
-                Err(err) => {
-                    error!(
-                            "Failed to update change network status, app_id: [{}], event: [{:?}], err: [{}]",
-                            app_id, event, err
-                        );
+    let mut tx = match db.connection_pool.begin().await {
+        Ok(tx) => tx,
+        Err(err) => {
+            error!(
+                "Failed to create new transaction to save change network event, app_id: [{}], event: [{:?}], err: [{}]",
+                app_id, event, err
+            );
+            return;
+        }
+    };
 
-                    // Rollback the transaction
-                    if let Err(err) = tx.rollback().await {
-                        error!(
-                                "Failed to rollback transaction for update change network event, app_id: [{}], event: [{:?}], err: [{}]",
-                                app_id, event, err
-                            );
-
-                        return;
-                    }
-                }
+    // Update the change network event in the database
+    match db
+        .update_event_change_network(
+            &mut tx,
+            &event.request_id,
+            RequestStatus::from(&event.failure_reason),
+            &event.new_network,
+        )
+        .await
+    {
+        Ok(_) => {
+            // Commit the transaction
+            if let Err(err) = tx.commit().await {
+                error!(
+                        "Failed to commit transaction for update change network event, app_id: [{}], event: [{:?}], err: [{}]",
+                        app_id, event, err
+                    );
             }
         }
         Err(err) => {
             error!(
-                    "Failed to create new transaction to update change network event, app_id: [{}], event: [{:?}], err: [{}]",
+                "Failed to update change network status, app_id: [{}], event: [{:?}], err: [{}]",
+                app_id, event, err
+            );
+
+            // Rollback the transaction
+            if let Err(err) = tx.rollback().await {
+                error!(
+                    "Failed to rollback transaction for update change network event, app_id: [{}], event: [{:?}], err: [{}]",
                     app_id, event, err
                 );
+            }
         }
     }
 }
