1b91d27328f675f4faac6679ed526a63f4d1ee9a
“Giems” <hubert.wabia@gmail.com>
2024-03-22 12:28:31 +0100

    remove constraint

--- database/migrations/0013_team_invites.sql
+++ database/migrations/0013_team_invites.sql
@@ -9,5 +9,3 @@ CREATE TABLE team_invites(
 
 CREATE INDEX team_invites_user_email_idx ON team_invites(user_email);
 CREATE INDEX team_invites_team_id_idx ON team_invites(team_id);
--- This will ensure that a user can only be invited to a team once
-CREATE UNIQUE INDEX team_invites_team_id_user_email_idx ON team_invites(team_id, user_email);
