44b47430a2d7120fec21ba12e8e4a21d75a8b0f2
Julia Seweryn <juliaseweryn17@gmail.com>
2024-08-28 12:30:58 +0200

    fixed

--- server/src/http/cloud/login/login_with_passkey_finish.rs
+++ server/src/http/cloud/login/login_with_passkey_finish.rs
@@ -23,7 +23,7 @@ pub struct HttpLoginWithPasskeyFinishRequest {
     #[garde(email)]
     pub email: String,
     #[garde(skip)]
-    pub passkey_credential: PublicKeyCredential,
+    pub credential: PublicKeyCredential,
     #[garde(skip)]
     pub enforce_ip: bool,
 }
@@ -64,7 +64,7 @@ pub async fn login_with_passkey_finish(
 
     // Finish passkey authentication
     if let Err(err) = web_auth.finish_passkey_authentication(
-        &request.passkey_credential,
+        &request.credential,
         &session_data.passkey_verification_state,
     ) {
         warn!(
