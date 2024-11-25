89c18a54fcae174380fd1b256786ea2ebf56cb3d
LukassF <szkulkerolf@gmail.com>
2024-11-25 17:41:21 +0100

    fix deeplinks on eager connect

--- sdk/packages/selector-sui/src/adapter.ts
+++ sdk/packages/selector-sui/src/adapter.ts
@@ -329,6 +329,26 @@ export class NightlyConnectSuiAdapter extends EventEmitter<SuiAdapterEvents> {
                   this.connecting = false
                   this._connectionType = ConnectionType.Nightly
                   this.setSelectedWallet({ isRemote: true })
+                  // we only run it to reassign deeplinks on eager connect (not on qr connection)
+                  if (isMobileBrowser()) {
+                    const wallet = this.walletsList.find((w) => w.name === 'Nightly')
+                    if (wallet?.mobile) {
+                      // If we have a native deeplink, we should use it
+                      if (wallet.mobile?.native !== null) {
+                        this._app.connectDeeplink({
+                          walletName: wallet.name,
+                          url: wallet.mobile.native
+                        })
+                      }
+                      // If we have a universal deeplink, we should use it
+                      else if (wallet.mobile?.universal !== null) {
+                        this._app.connectDeeplink({
+                          walletName: wallet.name,
+                          url: wallet.mobile.universal
+                        })
+                      }
+                    }
+                  }
                   this.emit('connect', this._accounts)
                   resolve()
                   return
