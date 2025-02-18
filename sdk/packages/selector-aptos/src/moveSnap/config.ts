6e7cb886687d92623d69ba48d299a4a463a525b7
LukassF <szkulkerolf@gmail.com>
2025-02-18 23:41:46 +0100

    fix snap config

--- sdk/packages/selector-aptos/src/moveSnap/config.ts
+++ sdk/packages/selector-aptos/src/moveSnap/config.ts
@@ -1,3 +1,3 @@
 export const defaultSnapOrigin =
   // eslint-disable-next-line no-restricted-globals
-  process.env.SNAP_ORIGIN ?? `local:http://localhost:8080`
+  process.env.SNAP_ORIGIN ?? `npm:@nightlylabs/metamask-move-snap`
